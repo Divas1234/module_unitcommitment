407e23f46842bc9387702a0acf03c78e03d71128
Divas1234 <45745675+Divas1234@users.noreply.github.com>
2025-04-29 23:14:19 +0800

    Refactor: update editor settings and include Benders multicuts expression in construct_multicuts_lib

--- Tools/construct_multicuts_lib/construct_multicuts.jl
+++ Tools/construct_multicuts_lib/construct_multicuts.jl
@@ -1,10 +1,12 @@
 include("_get_benders_multi_opti_feas_cuts.jl")
 include("_get_RhsCoeffi_in_DIFFconstraints.jl")
 include("_get_dual_subprob_constrs_coefficients.jl")
+include("_get_bd_multiplecut_expression.jl")
 
 export add_optimitycut_constraints!, add_feasibilitycut_constraints!, get_dual_constrs_coefficient
 export get_greater_than_constr_rhs, get_smaller_than_constr_rhs, get_equal_to_constr_rhs
 export get_x_coeff_vectors_from_constr, get_u_coeff_vectors_from_constr, get_v_coeff_vectors_from_constr
 export get_coeff_from_constr
+export get_benders_multicuts_expression
 
 println("\t\u2192 multicuts_libs have been loaded.")
